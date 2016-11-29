bbc4ea2549d71bc52602bbc302b469246ece2884
Geraldi Kusuma Arnanto <geraldi@kudo.co.id>
2016-11-29 16:30:38 +0700

    add UIView extension

--- POPTechTalk/POPButton.swift
+++ POPTechTalk/POPButton.swift
@@ -9,13 +9,5 @@
 import UIKit
 
 class POPButton: UIButton {
-    func shake() {
-        let animation = CABasicAnimation(keyPath: "position")
-        animation.duration = 0.05
-        animation.repeatCount = 5
-        animation.autoreverses = true
-        animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 4.0, y: self.center.y))
-        animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 4.0, y: self.center.y))
-        layer.add(animation, forKey: "position")
-    }
+
 }
--- POPTechTalk/ViewController.swift
+++ POPTechTalk/ViewController.swift
@@ -29,3 +29,15 @@ class ViewController: UIViewController {
 
 }
 
+extension UIView {
+    func shake() {
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.05
+        animation.repeatCount = 5
+        animation.autoreverses = true
+        animation.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 4.0, y: self.center.y))
+        animation.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 4.0, y: self.center.y))
+        layer.add(animation, forKey: "position")
+    }
+}
+
